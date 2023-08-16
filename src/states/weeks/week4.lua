ea15028d8ab168b0234ba45a577ecde163396fb5
GuglioIsStupid <bradsbluetab@gmail.com>
2023-08-16 19:39:30 -0400

    lil changes + next to no work on week4

--- src/states/weeks/week4.lua
+++ src/states/weeks/week4.lua
@@ -1,38 +1,13 @@
-local week3 = {}
+local week4 = {}
 local song = 1
 local difficulty
 
-local sky, city, cityWindows, behindTrain, street
-local winColors, winColor, winAlpha
-
-function week3:enter(from, song_, diff)
+function week4:enter(from, song_, diff)
     song = song_ or 1
     difficulty = diff or ""
     weeks:enter()
 
     enemy = love.filesystem.load("assets/sprites/week3/pico.lua")()
-
-    winColors = {
-        {49, 162, 253}, -- Blue
-        {49, 253, 140}, -- Green
-        {251, 51, 245}, -- Magenta
-        {253, 69, 49}, -- Orange
-        {251, 166, 51}, -- Yellow
-    }
-    winAlpha = 1
-    winColor = 1
-
-    sky = graphics.newImage(graphics.imagePath("stages/city/sky"))
-    city = graphics.newImage(graphics.imagePath("stages/city/city"))
-    cityWindows = graphics.newImage(graphics.imagePath("stages/city/city-windows"))
-    behindTrain = graphics.newImage(graphics.imagePath("stages/city/behind-train"))
-    street = graphics.newImage(graphics.imagePath("stages/city/street"))
-
-    sky.sizeX, sky.sizeY = 1.35, 1.35
-    city.sizeX, city.sizeY = 1.35, 1.35
-    cityWindows.sizeX, cityWindows.sizeY = 1.35, 1.35
-    behindTrain.sizeX, behindTrain.sizeY = 1.35, 1.35
-    street.sizeX, street.sizeY = 1.35, 1.35
 
     girlfriend.y = -15
     boyfriend.y = 35
@@ -42,12 +17,11 @@ function week3:enter(from, song_, diff)
 
     enemy.x = -85
     enemy.y = 25
-    enemy.sizeX = -1
 
     self:load()
 end
 
-function week3:load()
+function week4:load()
     if song == 1 then
         inst = love.audio.newSource("assets/songs/week3/pico/Inst.ogg", "stream")
         voices = love.audio.newSource("assets/songs/week3/pico/Voices.ogg", "stream")
@@ -62,7 +36,7 @@ function week3:load()
     self:initUI()
 end
 
-function week3:initUI()
+function week4:initUI()
     weeks:initUI()
 
     if song == 1 then
@@ -75,21 +49,14 @@ function week3:initUI()
     weeks:setupCountdown()
 end
 
-function week3:update(dt)
+function week4:update(dt)
     weeks:update(dt)
     weeks:updateEvents(dt)
 
-    if musicThres ~= oldMusicThres and math.fmod(absMusicTime, 240000 / bpm) < 100 then
-        winColor = winColor + 1
+    if song == 3 and musicTime > 56000 and musicTime < 67000 and musicThres ~= oldMusicThres and math.fmod(absMusicTime, 60000 / bpm) < 100 then
+        if camScaleTimer then Timer.cancel(camScaleTimer) end
 
-        if winColor > #winColors then
-            winColor = 1
-        end
-        winAlpha = 1
-    end
-
-    if winAlpha > 0 then
-        winAlpha = winAlpha - ((bpm/260) * dt)
+        camScaleTimer = Timer.tween((60 / bpm) / 16, camera, {zoom = camera.toZoom * 1.05}, "out-quad", function() camScaleTimer = Timer.tween((60 / bpm), camera, {sizeX = camera.toZoom}, "out-quad") end)
     end
 
     if not countingDown and not inst:isPlaying() then
@@ -102,26 +69,21 @@ function week3:update(dt)
     end
 end
 
-function week3:topDraw()
+function week4:topDraw()
     local curWinColor = winColors[winColor]
 
     love.graphics.push()
         love.graphics.translate(200,120)
-        --love.graphics.scale(camera.zoom*1.3, camera.zoom*1.3)
-        --love.graphics.translate(camera.x, camera.y)
 
         love.graphics.push()
             love.graphics.translate(camera.x * 0.25, camera.y * 0.25)
             love.graphics.scale(camera.zoom * 1.3, camera.zoom*1.3)
-            sky:draw()
         love.graphics.pop()
         
         love.graphics.push()
             love.graphics.translate(camera.x * 0.5, camera.y * 0.5)
             love.graphics.scale(camera.zoom * 1.3, camera.zoom*1.3)
-            city:draw()
-            graphics.setColor(curWinColor[1]/255,curWinColor[2]/255,curWinColor[3]/255, winAlpha)
-            cityWindows:draw()
+            
             graphics.setColor(1,1,1)
         love.graphics.pop()
         
@@ -129,8 +91,7 @@ function week3:topDraw()
         love.graphics.push()
             love.graphics.translate(camera.x * 0.9, camera.y * 0.9)
             love.graphics.scale(camera.zoom * 1.3, camera.zoom*1.3)
-            behindTrain:draw()
-            street:draw()
+            
             girlfriend:draw()
         love.graphics.pop()
 
@@ -146,15 +107,15 @@ function week3:topDraw()
     weeks:topDraw()
 end
 
-function week3:bottomDraw()
+function week4:bottomDraw()
     weeks:bottomDraw()
 end
 
-function week3:leave()
+function week4:leave()
     sky:release()
     city:release()
     cityWindows:release()
     weeks:leave()
 end
 
-return week3
+return week4
