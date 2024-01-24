19b203b6223ba56b1c53913788ca635e27737da9
GuglioIsStupid <bradsbluetab@gmail.com>
2024-01-24 15:35:20 -0500

    remove comments oops

--- tools/packer-to-xml/to-xml.py
+++ tools/packer-to-xml/to-xml.py
@@ -1,28 +1,6 @@
 import os, sys
 import xml.etree.ElementTree as ET
 
-# Packer atlas example layout:
-"""
-idle spirit_0 = 0 1028 128 128 # anim_name = x, y, w, h
-idle spirit_1 = 0 1157 128 128
-idle spirit_2 = 1542 0 128 128
-idle spirit_3 = 643 900 128 128
-spirit down_0 = 0 0 256 256
-spirit down_1 = 257 0 256 256
-spirit down_2 = 771 514 256 256
-
-# etc...
-
-"""
-# conver to adobe animate xml format
-"""
-<TextureAtlas imagePath="filename.png">
-    <!-- Created with Packer texture atlas generator-->
-    <SubTexture name="idle spirit_0" x="0" y="1028" width="128" height="128"/>
-    etc...
-</TextureAtlas>
-"""
-
 def main():
     file = sys.argv[1]
     
